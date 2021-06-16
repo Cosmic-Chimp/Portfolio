6cf4de7a27f81729e991c6da3821e3fda0723ee9
Rueben <75012722+Cosmic-Chimp@users.noreply.github.com>
2021-06-16 10:19:45 +0200

    Btn working now

--- src/main.js
+++ src/main.js
@@ -1,17 +1,4 @@
 //--- Hamburger Menu Animation Script ---//
-// import jQuery from 'jquery';
-// const $ = jQuery;
-
-// const { JSDOM } = require("jsdom");
-
-// require(JSDOM).env("", function(err, window) {
-//   if(err) {
-//     console.error(err);
-//     return;
-//   }
-
-//   var $ = require("jquery")(window);
-// })
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".menuItems");
@@ -19,16 +6,9 @@ const links = document.querySelectorAll(".menuItems .items");
 
 /// onclick animate and close ///
 
-let toggleMenu = () => {
-  //alert("hallo");
-  $(".icon").toggleClass("close");
-  links.toggleClass("open");
+  window.toggleMenu = () => {
+    // alert("hallo");
+    $(".icon").toggleClass("close");
+    links.toggleClass("open");
+
   };
-
-  // toggleMenu(); --> if uncommented-the func runs on page load,
-  //  but says toggleMenu() is not defined when not called like this 
-hamburger.click(function() {
-  toggleMenu();
-})
-
-// modules.exports = toggleMenu;
