dfab20fb418515a48eb360fc33baf6b04c63723d
Rueben <75012722+Cosmic-Chimp@users.noreply.github.com>
2021-06-19 16:02:40 +0200

    quick update
    carousel functionality and basic styling
    
    added pulse effect to view work btn

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -118,6 +118,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 //--- Hamburger Menu Animation Script ---//
 var hamburger = document.querySelector(".hamburger");
 var navLinks = document.querySelector(".menuItems");
@@ -130,7 +136,60 @@ window.toggleMenu = function () {
   links.forEach(function (link) {
     link.classList.toggle("fade");
   });
-};
+}; //---> CAROUSEL FUNCTIONALITY <---//
+
+
+var slidePosition = 0;
+var slides = document.getElementsByClassName('carousel__item');
+var totalSlides = slides.length; //console.log(totalslides)// 
+
+document.getElementById('carousel__button--next').addEventListener("click", function () {
+  moveToNextSlide();
+});
+document.getElementById('carousel__button--prev').addEventListener("click", function () {
+  moveToPrevSlide();
+});
+
+function updateSlidePosition() {
+  var _iterator = _createForOfIteratorHelper(slides),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var slide = _step.value;
+      slide.classList.remove('carousel__item--visible');
+      slide.classList.add('carousel__item--hidden');
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  slides[slidePosition].classList.add('carousel__item--visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+} // $('input').on('change', function() {
+//   $('body').toggleClass('blue');
+// });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/main.js
+++ src/main.js
@@ -14,3 +14,67 @@ const links = document.querySelectorAll(".menuItems li");
       link.classList.toggle("fade");
     });
   };
+
+  //---> CAROUSEL FUNCTIONALITY <---//
+
+  let slidePosition = 0;
+  const slides = document.getElementsByClassName('carousel__item');
+  const totalSlides = slides.length;
+  
+  //console.log(totalslides)// 
+  document.
+    getElementById('carousel__button--next')
+    .addEventListener("click", function() {
+      moveToNextSlide();
+    });
+  document.
+    getElementById('carousel__button--prev')
+    .addEventListener("click", function() {
+      moveToPrevSlide();
+    });
+  
+  function updateSlidePosition() {
+    for (let slide of slides) {
+      slide.classList.remove('carousel__item--visible');
+      slide.classList.add('carousel__item--hidden');
+    }
+  
+    slides[slidePosition].classList.add('carousel__item--visible');
+  }
+  
+  function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+      slidePosition = 0;
+    } else {
+      slidePosition++;
+    }
+  
+    updateSlidePosition();
+  }
+  
+  function moveToPrevSlide() {
+    if (slidePosition === 0) {
+      slidePosition = totalSlides - 1;
+    } else {
+      slidePosition--;
+    }
+  
+    updateSlidePosition();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // $('input').on('change', function() {
+  //   $('body').toggleClass('blue');
+  // });
